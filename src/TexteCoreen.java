06b96ea62a188891ed9c0990bb8ae85fbb478b12
maxwell <59898718+maxowell@users.noreply.github.com>
2020-03-06 13:17:27 -0500

    Creation repository

---
+++ src/TexteCoreen.java
@@ -0,0 +1,20 @@
+public class TexteCoreen {
+    public TexteCoreen(String texte ) {
+    }
+
+    public int noConsonneInitiale( int position ){
+        return 0;
+    }
+
+    public int noVoyelle( int position ) {
+        return 0;
+    }
+
+    public int noConsonneFinale( int position ) {
+        return 0;
+    }
+
+    public String traduire() {
+        return "";
+    }
+}
